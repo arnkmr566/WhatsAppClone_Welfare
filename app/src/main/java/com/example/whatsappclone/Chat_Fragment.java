eb5ab67a54d16983ec72f391eae0b423f07c0644
ARUN KUMAR <arnkmr566@gmail.com>
2022-03-14 10:03:30 +0530

    WhatsAppClone_Welfare

---
+++ app/src/main/java/com/example/whatsappclone/Chat_Fragment.java
@@ -0,0 +1,25 @@
+package com.example.whatsappclone;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Chat_Fragment extends Fragment {
+
+    public Chat_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_chat_, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/whatsappclone/FragmentAdapter.java
@@ -0,0 +1,56 @@
+package com.example.whatsappclone;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    public FragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0: return new Chat_Fragment();
+            case 1: return new Status_Fragment();
+            case 2: return new Calls_Fragment();
+
+            default:return new Chat_Fragment();
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        String title = null;
+
+        if (position == 0)
+        {
+            title = "CHATS";
+        }
+
+        else if (position == 1)
+        {
+            title = "STATUS";
+        }
+        else if (position == 2)
+        {
+            title = "CALLS";
+        }
+        return title;
+    }
+}
